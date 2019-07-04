dc8edf39ddeac3021b07df9e00b676ad7f847b4f
EndreoT <endreotm@gmail.com>
2019-07-03 18:53:03 -0700

    Updates

---
+++ precache-manifest.27ff52687f0c1579a6c6e3757e6d673b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c430e57f95ae1cd57bf996d810de5a48",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "71fafa4df8214559b166",
+    "url": "/clicky-game/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "cf6a6bc9ecddeacd1a71",
+    "url": "/clicky-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "71fafa4df8214559b166",
+    "url": "/clicky-game/static/js/2.1302bca6.chunk.js"
+  },
+  {
+    "revision": "cf6a6bc9ecddeacd1a71",
+    "url": "/clicky-game/static/js/main.92506240.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);

90382d6bdc710151cdbbd322d5f463b2da37b310
EndreoT <endreotm@gmail.com>
2019-07-07 13:37:17 -0700

    Updates

---
+++ precache-manifest.ff7ac58e5509631c6fb3c02ac39e0a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98382f4ae2cd131fbf7f3f9c75219ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f3752b2360f0f38df4d",
+    "url": "./static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5644fde09a84f56a9e19",
+    "url": "./static/css/main.a0a0430b.chunk.css"
+  },
+  {
+    "revision": "2f3752b2360f0f38df4d",
+    "url": "./static/js/2.f19c2b6d.chunk.js"
+  },
+  {
+    "revision": "5644fde09a84f56a9e19",
+    "url": "./static/js/main.2f24bfa9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "485468fefd85eea9c13e14f80e5595bb",
+    "url": "./static/media/background.485468fe.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.ca699654828c11146fcd155f15002ebc.js"
+  "./precache-manifest.ff7ac58e5509631c6fb3c02ac39e0a42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
